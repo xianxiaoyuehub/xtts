c9d3c541919fc1ca9a90dc83349f4039ef1bb660
xianyue <xianyue@stu.ahu.edu.cn>
2024-02-20 09:49:30 +0000

    none

--- TTS/tts/models/xtts.py
+++ TTS/tts/models/xtts.py
@@ -388,7 +388,11 @@ class Xtts(BaseTTS):
         gpt_his_cond = []
         for i in audio_paths:
             cond, _, _ = get_prompt_slice(
-                    i, self.config.max_conditioning_length, self.config.min_conditioning_length, 22050, True
+                    i, 
+                    self.config.max_conditioning_length, 
+                    self.config.min_conditioning_length, 
+                    self.config.input_sample_rate, 
+                    True
                 )
             gpt_his_cond.append(self.get_gpt_cond_latents(cond, load_sr, length=gpt_cond_len))
         gpt_his_cond = sum(gpt_his_cond)
